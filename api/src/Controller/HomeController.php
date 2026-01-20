06ba3f9d8e64e31c495fe0959e66ae1dc61ab3c2
maxc0434 <max.couillet@gmail.com>
2026-01-20 13:42:40 +0100

    homecontroller simple

--- api/src/Controller/HomeController.php
+++ api/src/Controller/HomeController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/api/home', name: 'app_home', methods: ['GET'])]
+    public function index(#[CurrentUser] ?User $user): JsonResponse
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $data = [
+            'app_name' => 'Rebel Refine API',
+            'status' => 'Online',
+            'version' => '1.0.0',
+            'authenticated' => ($user !== null),
+        ];
+
+        if ($user) {
+            $data['message'] = "Bonjour " . $user->getNickname() . " ! Ravi de vous revoir sur Rebel Refine.";
+            $data['user_details'] = [
+                'email' => $user->getUserIdentifier(),
+                'roles' => $user->getRoles(),
+            ];
+        } else {
+            $data['message'] = "Bienvenue ! Veuillez vous connecter pour accéder aux profils.";
+        }
+
+        return $this->json($data);
     }
 }
