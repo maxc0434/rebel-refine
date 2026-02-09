8edfca53aa52198e4f5d1224ac9737fd431781dd
maxc0434 <max.couillet@gmail.com>
2026-02-09 17:05:20 +0100

    erase table message, essai nouvelle table

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -66,17 +66,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\ManyToMany(targetEntity: self::class)]
     private Collection $favorites;
 
-    /**
-     * @var Collection<int, Message>
-     */
-    #[ORM\OneToMany(targetEntity: Message::class, mappedBy: 'utilisateur')]
-    private Collection $messages;
-
     public function __construct()
     {
         $this->userImages = new ArrayCollection();
         $this->favorites = new ArrayCollection();
-        $this->messages = new ArrayCollection();
     }
 
     public function getId(): ?int { return $this->id; }
@@ -178,33 +171,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    /**
-     * @return Collection<int, Message>
-     */
-    public function getMessages(): Collection
-    {
-        return $this->messages;
-    }
 
-    public function addMessage(Message $message): static
-    {
-        if (!$this->messages->contains($message)) {
-            $this->messages->add($message);
-            $message->setUtilisateur($this);
-        }
-
-        return $this;
-    }
-
-    public function removeMessage(Message $message): static
-    {
-        if ($this->messages->removeElement($message)) {
-            // set the owning side to null (unless already changed)
-            if ($message->getUtilisateur() === $this) {
-                $message->setUtilisateur(null);
-            }
-        }
-
-        return $this;
-    }
 }
